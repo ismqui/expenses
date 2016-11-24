5b8d3bd67fe12e63c9fe44d97f3ef5705e77fde8
ismqui <ismqui.git@gmail.com>
2016-11-24 13:40:13 +0100

    add create to market controller

--- web/controllers/market_controller.ex
+++ web/controllers/market_controller.ex
@@ -12,4 +12,18 @@ defmodule Expenses.MarketController do
     changeset =  Market.changeset(%Market{}, %{})
     render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"market" => market}) do
+    changeset = Market.changeset(%Market{}, market)
+
+    case Repo.insert(changeset) do
+      {:ok, _market} ->
+        conn
+        |> put_flash(:info, "New market created!")
+        |> redirect(to: market_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render("new.html", changeset: changeset)
+    end
+  end
 end
